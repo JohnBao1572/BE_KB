0b5b08b59b7fbdd174b711d132801db15a43f601
Bao <jonnguyen1572@gmail.com>
2024-12-11 10:27:27 +0700

    Doing regis custom hashpassword

--- src/controllers/customer.ts
+++ src/controllers/customer.ts
@@ -1,10 +1,29 @@
+import { hash } from "crypto";
+import CustomerModel from "../models/CustomModel";
+import { generatorRandomText } from "../utils/generatorRandomText";
+import bcrypt from 'bcrypt'
+
 const create = async (req:any, res:any)=>{
+    const body = req.body;
+
     try {
+        const customer = await CustomerModel.findOne({email: body.email});
+
+        if(customer){
+            throw new Error("Email had already");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+
+        const hashpassword = await bcrypt.hash( generatorRandomText(6), salt);
+
+        body.password = hashpassword; 
+
         res.status(200).json({
-            message: 'tadada',
+            message: 'dada',
             data: [],
         })
-    } catch (error) {
+    } catch (error:any) {
         console.log(error);
         res.status(404).json({
             message:error
